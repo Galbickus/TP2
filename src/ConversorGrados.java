4d4c6fafabd3bc84dd8d479ec493b2c9aab73e62
Veronica <vgalbicka@gmail.com>
2024-04-05 22:09:26 -0300

    Celsius a Fharenheit

---
+++ src/ConversorGrados.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/*¡Ahora te toca a ti! Práctica los conceptos que se enseñaron a lo largo de esta lección con el siguiente desafío:
+Escribe un programa que convierta una temperatura en grados Celsius a Fahrenheit. Utiliza variables para representar los valores de las temperaturas e imprime en la consola el valor convertido de Celsius a Fahrenheit.
+Consejo: La fórmula para convertir temperaturas de grados Celsius a Fahrenheit es: (temperatura * 1.8) + 32.
+Después de terminar, probar y verificar que tu programa se haya ejecutado correctamente, crea una variable entera para mostrar la temperatura en Fahrenheit sin decimales. Recuerda que probablemente necesitarás realizar un casting de valores.*/
+
+public class ConversorGrados {
+
+    double gradosCelsius = 9;
+    double gradosFharenheit = gradosCelsius * 1.8 + 32;
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,22 @@ public class Main {
 
         double media = (8.2 + 6.0 + 9.0) / 3;
         System.out.println(media);
-        
+
         String sinopsis = """
                 Matrix ees  uns película situada en un futuro distópico. 
                 La mejor película del fin del milenio según El New Yorker.
                 Fué lanzada en: 
                 """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
+
+        int clasificacion = (int) media / 2;
+        System.out.println("Estrellas para esta película: " + clasificacion);
+
+
+        double gradosCelsius = 9;
+        double gradosFharenheit = gradosCelsius * 1.8 + 32;
+
+        System.out.println(gradosCelsius + " grados Celsius equivalen a " + gradosFharenheit + " grados Fharenhait.");
 
     }
 }
