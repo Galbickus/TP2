b15f413655c38df27078a2faabc7404ca15700e2
Veronica <vgalbicka@gmail.com>
2024-04-05 21:33:42 -0300

    ADD

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido(a) a Screen Match");
@@ -8,8 +6,10 @@ public class Main {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double nota = 8.2;
+
         double media = (8.2 + 6.0 + 9.0) / 3;
         System.out.println(media);
+        
         String sinopsis = """
                 Matrix ees  uns película situada en un futuro distópico. 
                 La mejor película del fin del milenio según El New Yorker.
