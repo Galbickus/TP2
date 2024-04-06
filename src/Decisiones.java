8e4e33e8385a03076ee0df7a5414963906ef16a2
Veronica <vgalbicka@gmail.com>
2024-04-06 00:30:29 -0300

    test

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -2,7 +2,7 @@ public class Decisiones {
 
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElPlan = true;
+        boolean incluidoEnElPlan = false;
         double nota = 8.2;
         String tipoPlan = "plus";
 
@@ -11,7 +11,7 @@ public class Decisiones {
         }else{
             System.out.println("Películas retro que aún vale la pena ver");
         }
-        if (incluidoEnElPlan || tipoPlan == "plus"){
+        if (incluidoEnElPlan && tipoPlan == "plus"){
             System.out.println("Disfrute de su película");
         }else {
             System.out.println("Película no incluída en su plan actual");
