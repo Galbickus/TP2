118d34663262c79849e334f84c531ca3a36309ca
Veronica <vgalbicka@gmail.com>
2024-04-06 00:28:51 -0300

    Clase3 control de flujo

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -2,13 +2,19 @@ public class Decisiones {
 
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElPlan = true;
+        boolean incluidoEnElPlan = false;
         double nota = 8.2;
+        String tipoPlan = "plus";
 
         if (fechaDeLanzamiento >= 2022){
             System.out.println("Películas más populares");
         }else{
             System.out.println("Películas retro que aún vale la pena ver");
+        }
+        if (incluidoEnElPlan || tipoPlan == "plus"){
+            System.out.println("Disfrute de su película");
+        }else {
+            System.out.println("Película no incluída en su plan actual");
         }
     }
 }
